8c4dc984b78af2adf593efb53880107e13147edf
najmulmyself <najmul.myself@gmail.com>
2022-11-29 15:40:26 +0600

    login and reg ui fixed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,10 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:simple_commerce/const/appColors.dart';
+import 'package:simple_commerce/screens/registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,13 +14,254 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('Login Screen'),
-        ],
+      backgroundColor: AppColors.deepOrange,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 150.h,
+              width: ScreenUtil().screenWidth,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.light,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "Sign In",
+                      style: TextStyle(fontSize: 22.sp, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: ScreenUtil().screenWidth,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(28.r),
+                    topRight: Radius.circular(28.r),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20.w),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                        Text(
+                          "Welcome Back",
+                          style: TextStyle(
+                              fontSize: 22.sp, color: AppColors.deepOrange),
+                        ),
+                        Text(
+                          "Glad to see you back my buddy.",
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            color: Color(0xFFBBBBBB),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              height: 48.h,
+                              width: 41.w,
+                              decoration: BoxDecoration(
+                                  color: AppColors.deepOrange,
+                                  borderRadius: BorderRadius.circular(12.r)),
+                              child: Center(
+                                child: Icon(
+                                  Icons.email_outlined,
+                                  color: Colors.white,
+                                  size: 20.w,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10.w,
+                            ),
+                            Expanded(
+                              child: TextField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                  hintText: "test@gmail.com",
+                                  hintStyle: TextStyle(
+                                    fontSize: 14.sp,
+                                    color: Color(0xFF414041),
+                                  ),
+                                  labelText: 'EMAIL',
+                                  labelStyle: TextStyle(
+                                    fontSize: 15.sp,
+                                    color: AppColors.deepOrange,
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: AppColors.deepOrange),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10.h,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              height: 48.h,
+                              width: 41.w,
+                              decoration: BoxDecoration(
+                                  color: AppColors.deepOrange,
+                                  borderRadius: BorderRadius.circular(12.r)),
+                              child: Center(
+                                child: Icon(
+                                  Icons.lock_outline,
+                                  color: Colors.white,
+                                  size: 20.w,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10.w,
+                            ),
+                            Expanded(
+                              child: TextField(
+                                controller: _passwordController,
+                                obscureText: _obscureText,
+                                decoration: InputDecoration(
+                                  hintText: "password must be 6 character",
+                                  hintStyle: TextStyle(
+                                    fontSize: 14.sp,
+                                    color: Color(0xFF414041),
+                                  ),
+                                  labelText: 'PASSWORD',
+                                  labelStyle: TextStyle(
+                                    fontSize: 15.sp,
+                                    color: AppColors.deepOrange,
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: AppColors.deepOrange),
+                                  ),
+                                  suffixIcon: _obscureText == true
+                                      ? IconButton(
+                                          onPressed: () {
+                                            setState(
+                                              () {
+                                                _obscureText = false;
+                                              },
+                                            );
+                                          },
+                                          icon: Icon(
+                                            Icons.remove_red_eye,
+                                            color: AppColors.deepOrange,
+                                            size: 20.w,
+                                          ),
+                                        )
+                                      : IconButton(
+                                          onPressed: () {
+                                            setState(
+                                              () {
+                                                _obscureText = true;
+                                              },
+                                            );
+                                          },
+                                          icon: Icon(
+                                            Icons.visibility_off,
+                                            color: Colors.grey,
+                                            size: 20.w,
+                                          ),
+                                        ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 40.h,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 45.h,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // signUp();
+                            },
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18.sp),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.deepOrange,
+                              elevation: 3,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                        Wrap(
+                          children: [
+                            Text(
+                              "Don't have an account?",
+                              style: TextStyle(
+                                fontSize: 13.sp,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xFFBBBBBB),
+                              ),
+                            ),
+                            GestureDetector(
+                              child: Text(
+                                " Sign Up",
+                                style: TextStyle(
+                                  fontSize: 13.sp,
+                                  fontWeight: FontWeight.w600,
+                                  color: AppColors.deepOrange,
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                    builder: (context) => RegistrationScreen(),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:simple_commerce/const/appColors.dart';
+
+import 'login_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -8,13 +14,254 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('Registration Screen'),
-        ],
+      backgroundColor: AppColors.deepOrange,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 150.h,
+              width: ScreenUtil().screenWidth,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.light,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(fontSize: 22.sp, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: ScreenUtil().screenWidth,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.r),
+                    topRight: Radius.circular(30.r),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20.w),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                        Text(
+                          "Welcome Buddy",
+                          style: TextStyle(
+                            fontSize: 22.sp,
+                            color: AppColors.deepOrange,
+                          ),
+                        ),
+                        Text(
+                          "Glad to see you back my buddy.",
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            color: Color(0xFFBBBBBB),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              height: 48.h,
+                              width: 41.w,
+                              decoration: BoxDecoration(
+                                  color: AppColors.deepOrange,
+                                  borderRadius: BorderRadius.circular(12.r)),
+                              child: Center(
+                                child: Icon(
+                                  Icons.email_outlined,
+                                  color: Colors.white,
+                                  size: 20.w,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 15.w,
+                            ),
+                            Expanded(
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: "test@gamil.com",
+                                  hintStyle: TextStyle(
+                                    fontSize: 14.sp,
+                                    color: Color(0xFF414041),
+                                  ),
+                                  label: Text("EMAIL"),
+                                  labelStyle: TextStyle(
+                                    fontSize: 14.sp,
+                                    color: AppColors.deepOrange,
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: AppColors.deepOrange),
+                                  ),
+                                  // border: InputBorder.none, //remove border to see the outline | used above code label
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              height: 48.h,
+                              width: 41.w,
+                              decoration: BoxDecoration(
+                                color: AppColors.deepOrange,
+                                borderRadius: BorderRadius.circular(12.r),
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  Icons.lock_outline,
+                                  color: Colors.white,
+                                  size: 20.w,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 15.w,
+                            ),
+                            Expanded(
+                              child: TextField(
+                                obscureText: _obscureText,
+                                decoration: InputDecoration(
+                                  hintText: "password must be 6 characters",
+                                  hintStyle: TextStyle(
+                                    fontSize: 14.sp,
+                                    color: Color(0xFF414041),
+                                  ),
+                                  label: Text("PASSWORD"),
+
+                                  labelStyle: TextStyle(
+                                    fontSize: 14.sp,
+                                    color: AppColors.deepOrange,
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: AppColors.deepOrange),
+                                  ),
+                                  // border: InputBorder.none, //remove border to see the outline | used above code label
+                                  suffixIcon: _obscureText == true
+                                      ? IconButton(
+                                          icon: Icon(
+                                            Icons.remove_red_eye,
+                                            color: AppColors.deepOrange,
+                                          ),
+                                          onPressed: () {
+                                            setState(
+                                              () {
+                                                _obscureText = false;
+                                              },
+                                            );
+                                          },
+                                        )
+                                      : IconButton(
+                                          icon: Icon(
+                                            Icons.visibility_off,
+                                            color: Colors.grey,
+                                          ),
+                                          onPressed: () {
+                                            setState(
+                                              () {
+                                                _obscureText = true;
+                                              },
+                                            );
+                                          },
+                                        ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 40.h,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 45.h,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // signUp();
+                            },
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18.sp),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.deepOrange,
+                              elevation: 3,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                        ),
+                        Wrap(
+                          children: [
+                            Text(
+                              "Already have an account?",
+                              style: TextStyle(
+                                fontSize: 13.sp,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xFFBBBBBB),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LoginScreen(),
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 13.sp,
+                                  fontWeight: FontWeight.w600,
+                                  color: AppColors.deepOrange,
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
