f49a6eb21a587ab383778a58ef627ba3dfc0f066
najmulmyself <najmul.myself@gmail.com>
2023-02-02 00:30:17 +0600

    bottom navigation bar added

--- lib/screens/bottom_nav_controller.dart
+++ lib/screens/bottom_nav_controller.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class BottomNavController extends StatelessWidget {
+  const BottomNavController({super.key});
 
   @override
   Widget build(BuildContext context) {
